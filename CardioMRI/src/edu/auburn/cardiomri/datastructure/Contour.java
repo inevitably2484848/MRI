3187e3d8f57e82afb73b7a672aa7b9132e25b35b
Moniz Dharani <mhd0003@auburn.edu>
2015-02-09 13:29:10 -0600

    Initial attempt at showing contour lines
    It wasn't very effective...

---
+++ CardioMRI/src/edu/auburn/cardiomri/datastructure/Contour.java
@@ -0,0 +1,197 @@
+package edu.auburn.cardiomri.datastructure;
+
+import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.PathIterator;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+import java.util.List;
+import java.util.Vector;
+
+public class Contour implements Shape {
+
+    // XY coordinates of points the user clicked
+    private List<javafx.geometry.Point2D> controlPoints;
+
+    public Contour() {
+        // TODO Auto-generated constructor stub
+        controlPoints = SIMPLE_CONTOUR;
+    }
+
+    @Override
+    public boolean contains(Point2D p) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean contains(Rectangle2D r) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean contains(double x, double y) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean contains(double x, double y, double w, double h) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public Rectangle getBounds() {
+        int minX = Integer.MAX_VALUE;
+        int minY = Integer.MAX_VALUE;
+        int maxX = Integer.MIN_VALUE;
+        int maxY = Integer.MIN_VALUE;
+
+        for (javafx.geometry.Point2D point : controlPoints) {
+            minX = (int) Math.floor(Math.min(point.getX(), minX));
+            minY = (int) Math.floor(Math.min(point.getY(), minY));
+            maxX = (int) Math.ceil(Math.max(point.getX(), maxX));
+            maxY = (int) Math.ceil(Math.max(point.getY(), maxY));
+        }
+
+        return new Rectangle(minX, minY, (maxX - minX), (maxY - minY));
+    }
+
+    @Override
+    public Rectangle2D getBounds2D() {
+        double minX = Double.MAX_VALUE;
+        double minY = Double.MAX_VALUE;
+        double maxX = Double.MIN_VALUE;
+        double maxY = Double.MIN_VALUE;
+
+        for (javafx.geometry.Point2D point : controlPoints) {
+            minX = Math.min(point.getX(), minX);
+            minY = Math.min(point.getY(), minY);
+            maxX = Math.max(point.getX(), maxX);
+            maxY = Math.max(point.getY(), maxY);
+        }
+
+        return new Rectangle2D.Double(minX, minY, (maxX - minX), (maxY - minY));
+    }
+
+    @Override
+    public PathIterator getPathIterator(AffineTransform at) {
+        // Just for the spike
+        // TODO Don't ignore the transform
+        controlPoints = SIMPLE_CONTOUR;
+
+        return new PathIterator() {
+            private int index = 0;
+
+            @Override
+            public void next() {
+                index += 1; // Is this right?
+            }
+
+            @Override
+            public boolean isDone() {
+                // TODO Make sure controlPoints isn't null
+                return index > controlPoints.size();
+            }
+
+            @Override
+            public int getWindingRule() {
+                return WIND_NON_ZERO;
+            }
+
+            @Override
+            public int currentSegment(double[] coords) {
+                if (index == 0) {
+                    coords[0] = controlPoints.get(0).getX();
+                    coords[1] = controlPoints.get(0).getY();
+                    return SEG_MOVETO;
+                } else if ((index > 0) && (index < controlPoints.size())) {
+                    // First control point
+                    coords[0] = controlPoints.get(index - 1).getX();
+                    coords[1] = controlPoints.get(index - 1).getY();
+
+                    // Second control point
+                    coords[2] = controlPoints.get(index).getX();
+                    coords[3] = controlPoints.get(index).getY();
+                    return SEG_QUADTO;
+                } else if (index == controlPoints.size()) {
+                    // First control point
+                    coords[0] = controlPoints.get(index - 1).getX();
+                    coords[1] = controlPoints.get(index - 1).getY();
+
+                    // Second control point
+                    coords[2] = controlPoints.get(0).getX();
+                    coords[3] = controlPoints.get(0).getY();
+                    return SEG_QUADTO;
+                } else {
+                    return SEG_CLOSE;
+                }
+            }
+
+            @Override
+            public int currentSegment(float[] coords) {
+                if (index == 0) {
+                    coords[0] = (float) controlPoints.get(0).getX();
+                    coords[1] = (float) controlPoints.get(0).getY();
+                    return SEG_MOVETO;
+                } else if ((index > 0) && (index < controlPoints.size())) {
+                    // First control point
+                    coords[0] = (float) controlPoints.get(index - 1).getX();
+                    coords[1] = (float) controlPoints.get(index - 1).getY();
+
+                    // Second control point
+                    coords[2] = (float) controlPoints.get(index).getX();
+                    coords[3] = (float) controlPoints.get(index).getY();
+                    return SEG_QUADTO;
+                } else if (index == controlPoints.size()) {
+                    // First control point
+                    coords[0] = (float) controlPoints.get(index - 1).getX();
+                    coords[1] = (float) controlPoints.get(index - 1).getY();
+
+                    // Second control point
+                    coords[2] = (float) controlPoints.get(0).getX();
+                    coords[3] = (float) controlPoints.get(0).getY();
+                    return SEG_QUADTO;
+                } else {
+                    return SEG_CLOSE;
+                }
+            }
+        };
+    }
+
+    @Override
+    public PathIterator getPathIterator(AffineTransform at, double flatness) {
+        // TODO Don't ignore flatness
+        return getPathIterator(at);
+    }
+
+    @Override
+    public boolean intersects(Rectangle2D r) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean intersects(double x, double y, double w, double h) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public static final List<javafx.geometry.Point2D> SIMPLE_CONTOUR;
+
+    static {
+        SIMPLE_CONTOUR = new Vector<javafx.geometry.Point2D>();
+
+        // Add points (0, 0) and (10, 10)
+        SIMPLE_CONTOUR.add(new javafx.geometry.Point2D(400.0, 300.0));
+        SIMPLE_CONTOUR.add(new javafx.geometry.Point2D(500.0, 400.0));
+        SIMPLE_CONTOUR.add(new javafx.geometry.Point2D(600.0, 500.0));
+        SIMPLE_CONTOUR.add(new javafx.geometry.Point2D(600.0, 600.0));
+        SIMPLE_CONTOUR.add(new javafx.geometry.Point2D(600.0, 500.0));
+        SIMPLE_CONTOUR.add(new javafx.geometry.Point2D(500.0, 400.0));
+        SIMPLE_CONTOUR.add(new javafx.geometry.Point2D(400.0, 300.0));
+    }
+}
--- CardioMRI/src/edu/auburn/cardiomri/gui/ImageView.java
+++ CardioMRI/src/edu/auburn/cardiomri/gui/ImageView.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.event.MouseListener;
 import java.util.Observable;
+import java.util.Vector;
 
 import javax.swing.JPanel;
 
@@ -11,77 +12,84 @@ import com.pixelmed.dicom.DicomException;
 import com.pixelmed.display.SingleImagePanel;
 import com.pixelmed.display.SourceImage;
 
+import edu.auburn.cardiomri.datastructure.Contour;
 import edu.auburn.cardiomri.datastructure.DICOMImage;
 
 public class ImageView implements java.util.Observer {
-	
+
 	private JPanel panel;
 	private MouseListener mouseListener;
 	private SingleImagePanel sIP;
 
 	// Observer methods
+	@Override
 	public void update(Observable obs, Object obj) {
 		if (obj.getClass() == DICOMImage.class) {
-//System.out.println("ImageView : update with DICOMImage");
-			
+			// System.out.println("ImageView : update with DICOMImage");
+
 			this.panel.removeAll();
-			
+
 			this.sIP = null;
-			
+
 			DICOMImage dImage = ((DICOMImage) obj);
-			
+
 			SourceImage sImage = null;
 			try {
 				sImage = new SourceImage(dImage);
-				
+
 				this.sIP = new SingleImagePanel(sImage);
-				
+
+				Vector<Contour> contours = new Vector<Contour>();
+				contours.add(new Contour());
+				this.sIP.setPreDefinedShapes(contours);
+
 				this.panel.revalidate();
-				
-				
+
 			} catch (DicomException e) {
 				e.printStackTrace();
 			}
-			
+
 			this.sIP.addMouseListener(this.mouseListener);
-			
+
 			SingleImagePanel.deconstructAllSingleImagePanelsInContainer(this.panel);
 			this.panel.removeAll();
 			this.panel.add(sIP);
-			
+
 			this.panel.revalidate();
 		}
 	}
-	
+
 	// Setters
 	/*
 	 * Sets the class' mouseListener attribute.
-	 * 
-	 *  @param mL : MouseListener object that is used as the class'
-	 *  mouseListener attribute.
+	 *
+	 * @param mL : MouseListener object that is used as the class' mouseListener
+	 * attribute.
 	 */
 	public void setMouseListener(MouseListener mL) {
 		this.mouseListener = mL;
 	}
-	
+
 	// Getters
 	/*
 	 * Returns the class panel attribute.
-	 * 
-	 *  @return		The class' panel attribute.
+	 *
+	 * @return The class' panel attribute.
 	 */
-	public JPanel getPanel() { return this.panel; }
-	
+	public JPanel getPanel() {
+		return this.panel;
+	}
+
 	// Constructors
 	public ImageView() {
-//System.out.println("ImageView()");
-		
+		// System.out.println("ImageView()");
+
 		this.panel = new JPanel();
-		this.panel.setLayout(new GridLayout(1,1));
+		this.panel.setLayout(new GridLayout(1, 1));
 		this.panel.setBackground(Color.BLACK);
 		this.panel.setOpaque(true);
-		
+
 		this.panel.setVisible(true);
 	}
-	
+
 }
