8020371480d5387650349426ea702b3e997efa28
Moniz Dharani <mhd0003@auburn.edu>
2015-03-11 12:49:30 -0500

    Refactor and add better comments for Contour

--- CardioMRI/src/edu/auburn/cardiomri/datastructure/Contour.java
+++ CardioMRI/src/edu/auburn/cardiomri/datastructure/Contour.java
@@ -6,15 +6,14 @@ import java.awt.geom.AffineTransform;
 import java.awt.geom.PathIterator;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Vector;
 
-import toxi.geom.Spline2D;
-import toxi.geom.Vec2D;
+import edu.auburn.cardiomri.lib.ContourCalc;
 
 public class Contour implements Shape {
+
+    private boolean isClosedCurve = true;
 
     // XY coordinates of points the user clicked
     private List<javafx.geometry.Point2D> controlPoints;
@@ -97,16 +96,9 @@ public class Contour implements Shape {
     public PathIterator getPathIterator(AffineTransform at) {
         // System.out.println(at.getScaleX());
         // System.out.println(at.getScaleY());
-
-        // Just for the spike
         // TODO Don't ignore the transform
-        //controlPoints = SIMPLE_CONTOUR;
 
-        this.sortPoints(controlPoints);
-        generatedPoints = new Vector<javafx.geometry.Point2D>();
-        this.generate();
-
-        this.sortPoints(generatedPoints);
+        ContourCalc.generate(controlPoints, generatedPoints, this.isClosedCurve);
 
         return new PathIterator() {
             private int index = 0;
@@ -220,7 +212,7 @@ public class Contour implements Shape {
     }
 
     /**
-     * Returns a list of the control points
+     * Returns a copy of the list of the control points
      *
      * @return copy of the internal list
      */
@@ -229,108 +221,33 @@ public class Contour implements Shape {
     }
 
     /**
-     * Returns a list of the points generated to create a smooth curve
+     * Returns a copy list of the points generated to create a smooth curve.
+     * The {@link #generate} function is called before the points are returned.
      *
      * @return copy of the internal list
      */
     public List<javafx.geometry.Point2D> getGeneratedPoints() {
         // TODO Can this be changed so that it's only called when necessary?
-        this.generate();
+        ContourCalc.generate(controlPoints, generatedPoints, this.isClosedCurve);
         return new Vector<javafx.geometry.Point2D>(generatedPoints);
     }
 
     /**
-     * Connects the control points with a smooth curve and fills generatedPoints
-     * with points on that curve.
+     * Get whether this contour is a closed curve or open curve
+     * 
+     * @return boolean value is true if the curve is closed
      */
-    private void generate() {
-        // TODO: sortControlPoints()
-        // Find centroid, sort by angle to centroid
-
-        generatedPoints.clear();
-        if (controlPoints.size() < 2) {
-            // generatedPoints.addAll(controlPoints);
-            return;
-        }
-
-        Vec2D[] points = new Vec2D[controlPoints.size()];
-        javafx.geometry.Point2D temp;
-        for (int i = 0; i < controlPoints.size(); i++) {
-            temp = controlPoints.get(i);
-            points[i] = new Vec2D((float) temp.getX(), (float) temp.getY());
-        }
-
-        Spline2D spline = new Spline2D(points);
-        List<Vec2D> genPoints = spline
-                .getDecimatedVertices(SEPARATION_DISTANCE);
-        // System.err.println(genPoints.size());
-
-        for (Vec2D point : genPoints) {
-            generatedPoints.add(new javafx.geometry.Point2D(point.x, point.y));
-        }
+    public boolean isClosedCurve() {
+        return this.isClosedCurve;
     }
+
 
     /**
-     * Calculates the centroid by averaging the x,y coordinates in the list
+     * Set the curve as closed or open
+     * 
+     * @param isClosedCurve true if the curve is closed
      */
-    private javafx.geometry.Point2D calcCentroid(
-            List<javafx.geometry.Point2D> points) {
-
-        double averageX = 0, averageY = 0;
-        for (javafx.geometry.Point2D p : points) {
-            averageX += p.getX();
-            averageY += p.getY();
-        }
-        averageX /= points.size();
-        averageY /= points.size();
-
-        javafx.geometry.Point2D centroid = new javafx.geometry.Point2D(
-                averageX, averageY);
-        return centroid;
-    }
-
-    private void sortPoints(List<javafx.geometry.Point2D> points) {
-        if (points == null || points.size() < 2) {
-            return;
-        }
-        javafx.geometry.Point2D centroid = this.calcCentroid(points);
-
-        Collections.sort(points, new Comparator<javafx.geometry.Point2D>() {
-
-            /**
-             * Returns negative integer if point p1 is comes before point p2 in
-             * the contour?
-             */
-            @Override
-            public int compare(javafx.geometry.Point2D p1,
-                    javafx.geometry.Point2D p2) {
-                double thetaP1 = Math.atan2(p1.getX() - centroid.getX(),
-                        p2.getY() - centroid.getY());
-                double thetaP2 = Math.atan2(p2.getX() - centroid.getX(),
-                        p2.getY() - centroid.getY());
-                double delta = thetaP1 - thetaP2;
-                if (delta < 0.1)
-                    return 0;
-                return (int) Math.signum(delta);
-            }
-        });
-    }
-
-    public static final List<javafx.geometry.Point2D> SIMPLE_CONTOUR;
-    private static int SEPARATION_DISTANCE = 1; // Distance between each
-    // of the generated points
-
-    static {
-        SIMPLE_CONTOUR = new Vector<javafx.geometry.Point2D>();
-
-        SIMPLE_CONTOUR.add(new javafx.geometry.Point2D(500, 400));
-        SIMPLE_CONTOUR.add(new javafx.geometry.Point2D(570, 430));
-        SIMPLE_CONTOUR.add(new javafx.geometry.Point2D(600, 500));
-        SIMPLE_CONTOUR.add(new javafx.geometry.Point2D(570, 570));
-        SIMPLE_CONTOUR.add(new javafx.geometry.Point2D(500, 600));
-        SIMPLE_CONTOUR.add(new javafx.geometry.Point2D(430, 570));
-        SIMPLE_CONTOUR.add(new javafx.geometry.Point2D(400, 500));
-        SIMPLE_CONTOUR.add(new javafx.geometry.Point2D(430, 430));
-        SIMPLE_CONTOUR.add(new javafx.geometry.Point2D(500, 400));
+    public void setClosedCurve(boolean isClosedCurve) {
+        this.isClosedCurve = isClosedCurve;
     }
 }
---
+++ CardioMRI/src/edu/auburn/cardiomri/lib/ContourCalc.java
@@ -0,0 +1,144 @@
+package edu.auburn.cardiomri.lib;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Vector;
+
+import javafx.geometry.Point2D;
+import toxi.geom.Spline2D;
+import toxi.geom.Vec2D;
+
+public final class ContourCalc {
+    private static int SEPARATION_DISTANCE = 1; // Distance between each of the generated points
+
+    /**
+     * Calculates the centroid by averaging the x,y coordinates in the list
+     *
+     * @param points List of points used to find the centroid
+     */
+    public static Point2D calcCentroid(List<Point2D> points) {
+
+        double averageX = 0, averageY = 0;
+        for (Point2D p : points) {
+            averageX += p.getX();
+            averageY += p.getY();
+        }
+        averageX /= points.size();
+        averageY /= points.size();
+
+        Point2D centroid = new Point2D(
+                averageX, averageY);
+        return centroid;
+    }
+
+
+    /**
+     * Sort the points in radial(?) order.
+     *
+     * @param points List of points to sort
+     */
+    public static void sortPoints(List<Point2D> points) {
+        if (points == null || points.size() < 2) {
+            return;
+        }
+        Point2D centroid = ContourCalc.calcCentroid(points);
+
+        Collections.sort(points, new Comparator<Point2D>() {
+
+            /**
+             * Returns negative integer if point p1 is comes before point p2 in
+             * the contour?
+             */
+            @Override
+            public int compare(Point2D p1,
+                    Point2D p2) {
+                double thetaP1 = Math.atan2(p1.getX() - centroid.getX(),
+                        p2.getY() - centroid.getY());
+                double thetaP2 = Math.atan2(p2.getX() - centroid.getX(),
+                        p2.getY() - centroid.getY());
+                double delta = thetaP1 - thetaP2;
+                if (delta < 0.1)
+                    return 0;
+                return (int) Math.signum(delta);
+            }
+        });
+    }
+
+    /**
+     * Connects the control points with a smooth curve and fills generatedPoints
+     * with points on that curve.
+     * <br />
+     * <ul>
+     * <li> The controlPoints list should have at least 2 points.</li>
+     * <li> Control points are sorted, so the order of the points in controlPoints
+     * might change.</li>
+     * <li> The points in the generated list are separated by a distance of 1.</li>
+     * <li> When isClosed is true, the function generates a closed curve by
+     * sorting, then repeating the sorted list as the input for generating a
+     * curve.</li>
+     * </ul>
+     *
+     * @param controlPoints List with the control points
+     * @param generatedPoints List in which to put the generated points
+     * @param isClosed Whether the curve should be closed or left open
+     */
+    public static void generate(List<Point2D> controlPoints,
+            List<Point2D> generatedPoints, boolean isClosed) {
+        if (controlPoints == null) {
+            throw new NullPointerException("controlPoints list is null");
+        }
+        if (generatedPoints == null) {
+            throw new NullPointerException("generatedPoints list is null");
+        }
+
+        // TODO: sortControlPoints()
+
+        generatedPoints.clear();
+        if (controlPoints.size() < 2) {
+            return;
+        }
+
+        ContourCalc.sortPoints(controlPoints);
+
+        List<Point2D> rawPoints;
+        if (isClosed) {
+            rawPoints = ContourCalc.createRepeatedList(controlPoints);
+        } else {
+            rawPoints = controlPoints;
+        }
+
+        Vec2D[] points = new Vec2D[rawPoints.size()];
+        Point2D point2D;
+        for (int i = 0; i < rawPoints.size(); i++) {
+            point2D = rawPoints.get(i);
+            points[i] = new Vec2D((float) point2D.getX(), (float) point2D.getY());
+        }
+
+        Spline2D spline = new Spline2D(points);
+        List<Vec2D> genPoints = spline
+                .getDecimatedVertices(SEPARATION_DISTANCE);
+
+        for (Vec2D point : genPoints) {
+            generatedPoints.add(new Point2D(point.x, point.y));
+        }
+    }
+
+    /**
+     * Copies a list of points so that the order of the original list is
+     * preserved, but is repeated three times. This is used to create a curve
+     * that stays smooth through the first and last control points.
+     *
+     * @param points Sorted list of control points
+     * @return A list three times the size of the input
+     */
+    public static List<Point2D> createRepeatedList(List<Point2D> points) {
+        int timesToRepeat = 3;
+        List<Point2D> allPoints = new Vector<Point2D>(points.size() * timesToRepeat);
+
+        for (int i = 0; i < timesToRepeat; i++) {
+            allPoints.addAll(points);
+        }
+        return allPoints;
+    }
+}
