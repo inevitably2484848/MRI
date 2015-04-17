76d136fcfbf34cc07b304e50eac74498cd1bca70
Ben Gustafson <bengus28@gmail.com>
2015-04-17 13:58:15 -0500

    Update to image view

--- CardioMRI/src/edu/auburn/cardiomri/gui/views/ImageView.java
+++ CardioMRI/src/edu/auburn/cardiomri/gui/views/ImageView.java
@@ -67,7 +67,8 @@ public class ImageView extends SingleImagePanel implements ViewInterface, Observ
 		try {
 			ConstructImage sImg = new ConstructImage(dImage);
 			//TODO:update image for single image panel
-			 super(sImg);
+			dirtySource(sImg);
+			this.repaint();
 
 		} catch (DicomException e) {
 			e.printStackTrace();
