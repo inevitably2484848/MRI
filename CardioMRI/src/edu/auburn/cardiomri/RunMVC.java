44e85075d5c84746396b36554fe922c319d94172
Ben Gustafson <bengus28@gmail.com>
2015-04-25 13:11:45 -0500

    Run MVC java doc

--- CardioMRI/src/edu/auburn/cardiomri/RunMVC.java
+++ CardioMRI/src/edu/auburn/cardiomri/RunMVC.java
@@ -3,21 +3,23 @@ package edu.auburn.cardiomri;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
-import edu.auburn.cardiomri.datastructure.Study;
-import edu.auburn.cardiomri.gui.models.StartModel;
 import edu.auburn.cardiomri.gui.models.WorkspaceModel;
 import edu.auburn.cardiomri.gui.models.WorkspaceModel.State;
-import edu.auburn.cardiomri.gui.views.StartView;
 import edu.auburn.cardiomri.gui.views.WorkspaceView;
 
+/**
+ * Starts the program
+ * 
+ * Workspace model is the only GUI class that holds a study object
+ * Workspace View has different states which control what the GUI looks like. 
+ * 
+ * @author Senior Design Spring 2015 
+ *
+ */
 public class RunMVC {
 
     protected WorkspaceView workspaceView;
     protected WorkspaceModel workspaceModel;
-    protected StartView startView;
-    protected StartModel startModel;
-    protected Study study;
-
 
     public RunMVC() {
         try {
