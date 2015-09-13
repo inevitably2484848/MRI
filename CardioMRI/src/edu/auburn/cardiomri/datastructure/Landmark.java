09b1d0aa5105c175e4a2464c23150c94c737bea7
F Davis Quarles <fdq0001@auburn.edu>
2015-09-13 18:32:54 -0500

    Landmark JavaDoc Comments
    Documenting methods for the landmark class

--- CardioMRI/src/edu/auburn/cardiomri/datastructure/Landmark.java
+++ CardioMRI/src/edu/auburn/cardiomri/datastructure/Landmark.java
@@ -1,6 +1,4 @@
-/**
- * 
- */
+
 package edu.auburn.cardiomri.datastructure;
 
 
@@ -15,31 +13,61 @@ package edu.auburn.cardiomri.datastructure;
 public class Landmark {
 	private Vector3d coordinates;
 	private LandmarkType landmarkType;
-	
+	/**
+	 * Constructor with type and coordinates
+	 * @param typeIn Landmark type Enum
+	 * @param x double of X coordinates
+	 * @param y double of Y coordinates
+	 */
 	public Landmark(LandmarkType typeIn, double x, double y){
 		this();
 		landmarkType = typeIn;
 		coordinates = new Vector3d(x,y,0);
 	}
+	/** Constructor 
+	 * 
+	 * @param typeIn Landmark type
+	 */
 	public Landmark (LandmarkType typeIn){
 		this();
 		landmarkType = typeIn;
 	}
+	/**
+	 * Default constructor
+	 */
 	private Landmark(){
 		
 	}
+	/**
+	 * Set a Landmarks coordinates
+	 * @param x double X coordinates
+	 * @param y double Y coordinates
+	 */
 	public void setLandmarkCoordinates(double x, double y){
 		coordinates = new Vector3d(x,y,0);
 		System.out.println("landmarks coordinates set: " + this.toString());
 	}
+	/**
+	 * get a landmarks coordinates
+	 * @return Vector3d Landmarks coordinates
+	 */
 	public Vector3d getCoordinates(){
 		return coordinates;
 	}
+	/**
+	 * formats a landmark as a string
+	 * @return String
+	 */
 	public String toString(){
 		String out = "[" + coordinates.getX() + "," + coordinates.getY() + "]";
 		return out;
 	}
 	//type Enum
+	/**
+	 * Enum of Landmark types
+	 * @author davis
+	 *
+	 */
 	public enum LandmarkType{
 	ARV,
 	IRV,
