7927d7ccd2d2606d39862eb0e483b09e6b0f2f8a
kullen <kullen@auburn.edu>
2015-09-27 18:13:30 -0500

    delete control points test

---
+++ CardioMRI/test/edu/auburn/cardiomri/datastructure/DeleteControlPointTest.java
@@ -0,0 +1,59 @@
+package edu.auburn.cardiomri.datastructure;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DeleteControlPointTest {
+
+    private Contour contour;
+    private List<Vector3d> controlPoints;
+    
+    @Before
+    public void setUp() {
+        contour = new Contour(Contour.Type.DEFAULT);
+        contour.addControlPoint(1, 2);
+        contour.addControlPoint(5, 8);
+        
+        controlPoints = contour.getControlPoints();
+       
+    }
+
+    @After
+    public void tearDown() {
+        contour = null;
+    }
+	
+    @Test
+    public void testDeleteControlPoint() {
+    	assertTrue(contour.deleteControlPoint(1, 2));
+    }
+    
+    
+    @Test
+    public void testAddCPThatIsToCloseToAnotherCPWillNotAdd() {
+    	int beforeSize = controlPoints.size();
+    	
+    	contour.addControlPoint(2,2);
+    	
+    	int afterSize = controlPoints.size();
+    	
+    	assertEquals(beforeSize, afterSize);
+    }
+    
+    @Test
+    public void testDeleteControlPointThatsNotThere(){
+    	
+    	boolean expected = false;
+    	boolean actual = contour.deleteControlPoint(9, 8);
+    	
+    	assertEquals(expected, actual);
+    }
+    
+    
+
+}
