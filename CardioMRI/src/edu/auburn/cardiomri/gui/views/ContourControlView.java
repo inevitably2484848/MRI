b2bff702fec93200626e188c0238b64d72532112
kullen <kullen@auburn.edu>
2015-09-27 18:09:28 -0500

    Attempted to implement Annotation Control: Back Burner
    ran into errors trying to update the bottom right panel to have a
    list of annotations for the current image. revalidate and repaint
    were not working. I even tried calling the panel from another class
    to update it. FAILED. I can print the list of contours on an image
    in the class but it won't update the panel. the Logic is there.
    If we have more time I will look at it later.

--- CardioMRI/src/edu/auburn/cardiomri/gui/views/ContourControlView.java
+++ CardioMRI/src/edu/auburn/cardiomri/gui/views/ContourControlView.java
@@ -34,70 +34,68 @@ public class ContourControlView extends View {
 	
 	public ContourControlView(DICOMImage dImage)
 	{
-		//super(); // *
+		super(); // *
 		ccv = this.panel;
 	    ccv.setOpaque(true);  // * 
 	    ccv.setVisible(true); // *
 	    
-	    
-		String fileName = "Title";
-		DefaultListModel<Contour> model = new DefaultListModel<Contour>();
-		if(dImage != null){
-			//System.out.println("DICOM NOT NULL");
-			 fileName = dImage.toString();
-			System.out.println(dImage.toString());
-			Vector<Contour> contours = dImage.getContours();
-			if(!(contours.isEmpty())){
-				for(Contour x : contours){
-					model.addElement(x);
-					System.out.println(x);
-				}
-			}
-			else { 
-				//System.out.println("NO CONTOURS");
-			}
-		}
-		else{  //TESTING
-			
-			//System.out.println("DICOM NULL");
-			
-			Vector<Contour> contours = new Vector<Contour>();
-			contours.add(new Contour(Contour.getTypeFromInt(1)));
-			contours.add(new Contour(Contour.getTypeFromInt(2)));
-			if(!(contours.isEmpty())){
-				for(Contour x : contours){
-					model.addElement(x);
-				}
-			}
-		} //END TESTING
-		
-		JList<Contour> list = new JList<Contour>(model);
-		list.setVisibleRowCount(5);
-		JScrollPane scroll = new JScrollPane(list);
-		scroll.setName("SCROLL PANE");
-		
-		ccv.setLayout(new BorderLayout());
-		
-		JPanel titlePanel = new JPanel();
-		JLabel title = new JLabel(fileName);
-		titlePanel.add(title);
-		titlePanel.setName("TITLE PANEL");
-		ccv.add(titlePanel, BorderLayout.NORTH);
-		//ccv.add(new JButton("TEST"), BorderLayout.SOUTH);
-		ccv.add(scroll, BorderLayout.CENTER);
-	    
-		
-		ccv.setPreferredSize(new Dimension(100,200));
-	    System.out.println("Set Panel Name to CCVPanel");
-	    ccv.setName("CCVPanel");
-	    ccv.updateUI();
-	    ccv.doLayout();
-		ccv.revalidate();
-		ccv.repaint();
-		
-		
+//	    ccv = this.panel;
+//		String fileName = "Title";
+//		DefaultListModel<Contour> model = new DefaultListModel<Contour>();
+//		if(dImage != null){
+//			//System.out.println("DICOM NOT NULL");
+//			 fileName = dImage.toString();
+//			System.out.println(dImage.toString());
+//			Vector<Contour> contours = dImage.getContours();
+//			if(!(contours.isEmpty())){
+//				for(Contour x : contours){
+//					model.addElement(x);
+//					System.out.println(x);
+//				}
+//			}
+//			else { 
+//				//System.out.println("NO CONTOURS");
+//			}
+//		}
+//		else{  //TESTING
+//			
+//			//System.out.println("DICOM NULL");
+//			
+//			Vector<Contour> contours = new Vector<Contour>();
+//			contours.add(new Contour(Contour.getTypeFromInt(1)));
+//			contours.add(new Contour(Contour.getTypeFromInt(2)));
+//			if(!(contours.isEmpty())){
+//				for(Contour x : contours){
+//					model.addElement(x);
+//				}
+//			}
+//		} //END TESTING
+//		
+//		JList<Contour> list = new JList<Contour>(model);
+//		list.setVisibleRowCount(5);
+//		JScrollPane scroll = new JScrollPane(list);
+//		scroll.setName("SCROLL PANE");
+//		
+//		ccv.setLayout(new BorderLayout());
+//		
+//		JPanel titlePanel = new JPanel();
+//		JLabel title = new JLabel(fileName);
+//		titlePanel.add(title);
+//		titlePanel.setName("TITLE PANEL");
+//		ccv.add(titlePanel, BorderLayout.NORTH);
+//		//ccv.add(new JButton("TEST"), BorderLayout.SOUTH);
+//		ccv.add(scroll, BorderLayout.CENTER);
+//	    
+//		
+//		ccv.setPreferredSize(new Dimension(100,200));
+//	    System.out.println("Set Panel Name to CCVPanel");
+//	    ccv.setName("CCVPanel");
+//	    ccv.updateUI();
+//	    ccv.doLayout();
+//		ccv.revalidate();
+//		ccv.repaint();
+//		
+//		
 	}	
-	
 
-	
 }
