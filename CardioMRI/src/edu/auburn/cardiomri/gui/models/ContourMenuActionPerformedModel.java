93c25baae794f57c501bc4399f7e235b819fb7ed
kullen <kullen@auburn.edu>
2015-10-05 18:46:29 -0500

    Action Listener for Contour MenuBar
    removed Action listener from ImageView and made new
    Class. when you select something in the Contour
    JMenuBar this action listener is called

---
+++ CardioMRI/src/edu/auburn/cardiomri/gui/models/ContourMenuActionPerformedModel.java
@@ -0,0 +1,94 @@
+package edu.auburn.cardiomri.gui.models;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Vector;
+
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+import edu.auburn.cardiomri.datastructure.Contour;
+import edu.auburn.cardiomri.datastructure.Contour.Type;
+import edu.auburn.cardiomri.gui.views.ImageView;
+import edu.auburn.cardiomri.gui.views.View;
+
+public class ContourMenuActionPerformedModel extends View implements ActionListener {
+	
+	protected JPanel imageContourPanel;
+	
+	@Override
+	public void actionPerformed(ActionEvent actionEvent) {
+		
+		String actionCommand = actionEvent.getActionCommand();
+		
+		if (actionCommand.equals("Delete Contour")) {
+			if (getImageModel().getSelectedContour() == null) {
+				JOptionPane.showMessageDialog(imageContourPanel,
+                "Please select a contour to delete.");
+			} 
+			else {
+				getImageModel().deleteSelectedContour();
+			}
+		} 
+		else if (actionCommand.equals("Hide Contour")) {
+			if (getImageModel().getSelectedContour() == null) {
+				JOptionPane.showMessageDialog(imageContourPanel,
+                "Please select a contour to hide.");
+			} 
+			else {
+				getImageModel().hideSelectedContour();
+			}
+		} 
+		else if (actionCommand.equals("Select Contour")) {
+			Vector<Contour> visibleContours = getImageModel()
+            .getVisibleContours();
+			if (visibleContours.size() > 0) {
+				Contour[] contours = new Contour[visibleContours.size()];
+				visibleContours.toArray(contours);
+
+				Contour c = (Contour) JOptionPane.showInputDialog(
+                imageContourPanel, "Select Contour: ", "Contours",
+                JOptionPane.PLAIN_MESSAGE, null, contours, "ham");
+
+				getImageModel().setSelectedContour(c);
+			} 
+			else {
+				JOptionPane.showMessageDialog(imageContourPanel,
+                "There are no visible contours to select.");
+			}
+		} 
+		else if (actionCommand.equals("Hide Contours")) {
+			if (getImageModel().getContours() == null || getImageModel().getContours().size() == 0) {
+				JOptionPane.showMessageDialog(imageContourPanel,
+                "There are no contours to hide.");
+			} else {
+				getImageModel().hideAllContours();
+			}
+		} 
+		else if (actionCommand.equals("Show Contours")) {
+			if (getImageModel().getHiddenContours() == null || getImageModel().getHiddenContours().size() == 0) {
+				JOptionPane.showMessageDialog(imageContourPanel,
+                "There are no contours to show.");
+			} 
+			else {
+				getImageModel().showAllContours();
+			}
+		} 
+		else if (actionCommand.equals("Delete All Contours")) {
+			if (getImageModel().getContours() == null || getImageModel().getContours().size() == 0) {
+				JOptionPane.showMessageDialog(imageContourPanel,"There are no contours to delete.");
+			} 
+			else {
+				getImageModel().deleteAllContours();
+			}
+		}
+	}
+		/**
+		 * gets Image model to add contour type
+		 * @return
+		 */
+		public static ImageModel getImageModel(){
+	    	return ImageView.getImageModelStatic();
+	    }
+	
+}
--- CardioMRI/src/edu/auburn/cardiomri/util/Mode.java
+++ CardioMRI/src/edu/auburn/cardiomri/util/Mode.java
@@ -1,4 +1,11 @@
 package edu.auburn.cardiomri.util;
+/**
+ * Mode is used on the mouseclick listen. 
+ * The action performed from a mouse click is based 
+ * on the mode that you are in
+ * @author Kullen
+ * @version 10/5/2015
+ */
 
 public class Mode {
 	protected static int mode = 0;   //0 - select ; 1 - contour ; 2 - landmark // kw
@@ -6,28 +13,23 @@ public class Mode {
 	protected static final int CONTOUR_MODE = 1;
 	protected static final int LANDMARK_MODE = 2;
 	
-	
-	 /** -----------------------------------------------------------------------
-     *  Static Method so Other Classes can get the Mode Value
-     *  @return mode - int
-     *  @author KulW
-     */
     public static int getMode(){
     	return mode;
     }
     
+   // to set the mode in another class Mode.setMode(Mode.contourMode()) 
     public static void setMode(int modeIN){
     	mode = modeIN;
     }
     
     public static int contourMode(){
-    	return 1;
+    	return CONTOUR_MODE;
     }
     public static int landmarkMode(){
-    	return 2;
+    	return LANDMARK_MODE;
     }
     public static int selectMode(){
-    	return 0;
+    	return SELECT_MODE;
     }
 
 	
