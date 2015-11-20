be3bfa29f5101c3510bbbf1095fc196bf9d579fd
Joshua <jkf0007@auburn.edu>
2015-11-19 20:50:49 -0600

    Committing is important

--- CardioMRI/src/edu/auburn/cardiomri/gui/views/ImageView.java
+++ CardioMRI/src/edu/auburn/cardiomri/gui/views/ImageView.java
@@ -191,6 +191,34 @@ public class ImageView extends SingleImagePanel implements ActionListener,
 			            	colorShape(tensionPoint2Ellipse, tensionPoint2.getColor());
 		            	}
 		            }
+		            
+//		            for(int i = 0; i < contour.getControlPoints().size(); i++) {
+//		            	
+//		            	ControlPoint controlPoint = contour.getControlPoints().get(i);
+//		            	
+//		            	Ellipse2D controlPointEllipse = new Ellipse2D.Double(controlPoint.getX(),
+//		                        controlPoint.getY(), 2, 2);
+//		            	
+//		            	TensionPoint tensionPoint1 = controlPoint.getTension1();
+//		            	TensionPoint tensionPoint2 = controlPoint.getTension2();
+//		            	
+//		            	Ellipse2D tensionPoint1Ellipse = new Ellipse2D.Double(tensionPoint1.getX(), tensionPoint1.getY(), 2, 2);
+//		    			Ellipse2D tensionPoint2Ellipse = new Ellipse2D.Double(tensionPoint2.getX(), tensionPoint2.getY(), 2, 2);
+//		    			
+//		            	colorShape(controlPointEllipse, controlPoint.getColor());
+//		            	if(contour.getControlPoints().size() > 1) {
+//		            		//check to not draw tension points of the final curve if the bezier curve is open
+//		            		if(i == 0 || i == (contour.getControlPoints().size() - 1)) {
+//		            			if(contour.isClosedCurve()) {
+//		            				colorShape(tensionPoint1Ellipse, tensionPoint1.getColor());
+//		            				colorShape(tensionPoint2Ellipse, tensionPoint2.getColor());
+//		            			}
+//		            		} else {
+//	            				colorShape(tensionPoint1Ellipse, tensionPoint1.getColor());
+//	            				colorShape(tensionPoint2Ellipse, tensionPoint2.getColor());
+//		            		}
+//		            	}
+//		            }
 	    		}
     		}
         }
